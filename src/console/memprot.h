5fc74ca0bee176fd424a322b6cb5816d220438d1
imbluedabr <baldergroen@gmail.com>
2026-02-05 23:17:26 +0100

    started work on a memory protection feature to catch stack corruption

---
+++ src/console/memprot.h
@@ -0,0 +1,10 @@
+#include <stdint.h>
+
+extern uint32_t protection_region_base;
+extern uint32_t protection_region_top;
+extern struct RV32IZicsr_State state;
+
+void memprot_tick();
+
+void memprot(char* cmd);
+void memunprot();
